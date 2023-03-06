0950bd60e275a3d3fd5dc83563009fd26df07de7
arturkalimullin <noti1996@gmail.com>
2023-03-06 14:12:00 +0300

    Add whitenoise

--- qortexProject/settings.py
+++ qortexProject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'whitenoise',
 ]
 
 MY_APPS = [
@@ -61,6 +62,7 @@ INSTALLED_APPS += DOC_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
